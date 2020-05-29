7587b961239512ea064a348e79939b886cac94e2
John Doan <doanja@plu.edu>
2020-05-29 15:50:12 -0700

    updated card styles

--- src/components/RecipePreview.tsx
+++ src/components/RecipePreview.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Row, Col, Button } from 'react-bootstrap';
 import '../App.css';
+import Ingredient from './Ingredient';
 
 interface RecipePreview {
   recipe: Recipe;
@@ -8,25 +9,32 @@ interface RecipePreview {
 }
 
 const RecipePreview: React.FC<RecipePreview> = ({ recipe, loadSingleRecipe }) => {
+  const [ingredients, setIngredients] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    setIngredients(recipe.extendedIngredients.map(ingredient => ingredient.name));
+  }, [recipe]);
+
   return (
-    <Card className='mt-3 shadow-sm recipe-details' bg='dark' text='light'>
+    <Card className='mt-3 card-shadow recipe-details' bg='dark' text='light'>
       <Card.Header>
         {recipe.title} - {`Ready in ${recipe.cookingMinutes} minutes`}
       </Card.Header>
       <Row noGutters={true}>
         <Col md={4} className='recipe-image card-bg'>
-          <img src={recipe.image} className='float-left mr-3' />
+          <img src={recipe.image} />
         </Col>
         <Col md={8} className='card-bg'>
-          <Card.Body>
+          <Card.Body className='text-dark'>
             <Card.Title>
-              {`Prep. Time: ${recipe.cookingMinutes}(s) - Serves: ${recipe.servings}`}
+              {`Prep. Time: ${recipe.cookingMinutes} minute(s) - Serves: ${recipe.servings}`}
             </Card.Title>
             <Card.Text>
-              {`Ingredients: marshmallow fluff, nutella, pound cake, unsalted butter`}
+              <h6>Ingredients: </h6>
             </Card.Text>
+            <Card.Text>{`${ingredients.join(', ')}`}</Card.Text>
             <Button
-              variant='light'
+              variant='dark'
               className='px-2 recipe-details'
               onClick={() => loadSingleRecipe(recipe)}>
               View Recipe Details
