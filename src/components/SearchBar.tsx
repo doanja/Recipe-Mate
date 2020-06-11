60a1ef00a557a86e03d46594f6a4be49437e3cf2
John Doan <doanja@plu.edu>
2020-06-10 17:23:12 -0700

    updating form

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, ChangeEvent } from 'react';
-import { InputGroup, Button, FormControl } from 'react-bootstrap';
+import { Form, InputGroup, Button, FormControl, FormGroup } from 'react-bootstrap';
 
 interface SearchBarProps {
   getRecipe: getRecipe;
@@ -8,24 +8,27 @@ interface SearchBarProps {
 const SearchBar: React.FC<SearchBarProps> = ({ getRecipe }) => {
   const [input, setInput] = useState('');
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
   const handleSubmit = () => {
+    console.log(input);
     getRecipe(input);
     setInput('');
   };
 
   return (
-    <InputGroup className='mt-3'>
-      <FormControl placeholder='Search Recipe' value={input} onChange={handleChange} />
-      <InputGroup.Append>
-        <Button variant='dark' onClick={handleSubmit}>
-          Search
-        </Button>
-      </InputGroup.Append>
-    </InputGroup>
+    <Form onSubmit={handleSubmit}>
+      <InputGroup className='mt-3'>
+        <Form.Control type='text' placeholder='Search a recipe' value={input} onChange={setInput} />
+        <InputGroup.Append>
+          <Button variant='dark' type='submit' onClick={handleSubmit}>
+            Search
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </Form>
   );
 };
 
