e971f72153687167cfc45145dea20df46e349ae9
John Doan <doanja@plu.edu>
2020-06-12 15:05:05 -0700

    fixed issue where bootstrap default scss was overriding custom scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/main.min.css';
 import './styles/reset.min.css';
 import App from './App';
-
-// Importing the Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
