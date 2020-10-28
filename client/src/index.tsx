a505d537c810dd265765080870479704bd6cc92b
doanja <doanja@plu.edu>
2020-10-28 12:21:08 -0700

    updated file import store in index

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 // redux
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import Store from './redux/store';
 
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +12,7 @@ import './styles/main.min.css';
 import './styles/reset.min.css';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <App />
   </Provider>,
   document.getElementById('root')
