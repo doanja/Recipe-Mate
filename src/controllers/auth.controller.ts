5d8003d357c96f629028b85d1c279bc87c4559b0
doanja <doanja@plu.edu>
2020-10-28 12:13:56 -0700

    fixed bug where auth.controller did not sign a valid user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -13,7 +13,7 @@ export const initSignupStrategy = (): Strategy => {
 
       if (user) return done(null, false, { message: 'That email is already taken.' });
       else {
-        const newUser = { email, password: hashSync(password, genSaltSync(8)) };
+        const newUser = { email, password: hashSync(password, genSaltSync(8)), recipes: [] };
         User.create(newUser);
 
         return done(null, newUser);
